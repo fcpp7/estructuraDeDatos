21e34e9ac1ca155b5951ba6e67c5730bb894e7b8
fcpp7 <facundo.perez@est.fi.uncoma.edu.ar>
2022-04-06 12:29:35 -0300

    Implementación cola estática

---
+++ lineales/lineales/estaticas/Cola.java
@@ -0,0 +1,132 @@
+package lineales.estaticas;
+
+public class Cola {
+
+    private Object[] arreglo;
+    private int frente;
+    private int fin;
+    private static final int TAMANIO = 10;
+
+    public Cola(){
+        this.arreglo = new Object[TAMANIO];
+        this.frente = 0;
+        this.fin = 0;
+
+    }
+
+
+    // Métodos modificadores
+
+    public boolean poner(Object elemento){
+        // Método que pone un elemento en el fin del arreglo y modifica la variable fin.
+        boolean exito = true;
+
+        if(this.esCompleta()){
+            // Si la pila esta completa, no puedo agregar elementos.
+            exito = false;
+        } else {
+            // Si la pila tiene lugar, agrego el elemento en la posición "fin" y corro fin 1 lugar.
+            this.arreglo[this.fin] = elemento;
+            this.fin = (this.fin+1)%this.TAMANIO;
+        }
+
+
+        return exito;
+    }
+
+
+    public boolean sacar(){
+        // Método que saca el primer elemento del arreglo y modifica la variable frente.
+        boolean exito = true;
+
+        if(this.esVacia()){
+            exito = false;
+        } else {
+            this.arreglo[this.frente] = null;
+            this.frente = (this.frente + 1) % this.TAMANIO;
+        }
+        return exito;
+    }
+    
+
+
+    // Métodos observadores
+
+    public Object obtenerFrente(){
+        // Método que retorna el elemento que esta en el frente, es decir el primero 
+        // que entró a la cola.
+
+        return this.arreglo[this.frente];
+
+    }
+
+    public boolean esVacia(){
+        // Método que devuelve true si la pila esta vacía y false en caso contrario.
+
+        return this.frente == this.fin;
+
+    }
+
+    public void vaciar(){
+
+        while(!this.esVacia()){
+            this.arreglo[this.frente] = null;
+            this.frente = (this.frente + 1) % this.TAMANIO;
+        }
+
+
+    }
+
+    public boolean esCompleta(){
+        // Método que devuelve true si la pila esta completa y false en caso contrario.
+
+        return this.frente == (this.fin+1)%this.TAMANIO;
+
+    }
+
+    // Métodos NO básicos
+
+    @Override
+    public Cola clone(){
+        // Método para clonar la cola. Crea la nueva cola, le pasa los parametros (frente y fin)
+        // y luego clona el arreglo original en la nueva cola clonada.
+
+        Cola nuevaCola = new Cola();
+
+        if(!this.esVacia()){
+            nuevaCola.frente = this.frente;
+            nuevaCola.fin = this.fin;
+            nuevaCola.arreglo = this.arreglo.clone();
+        }
+        return nuevaCola;
+    }
+
+
+    @Override
+    public String toString() {
+        // Método para debbuging, devuelve un print de la cola.
+
+        String s;
+        if(this.esVacia()){
+
+            s = "Pila vacía";
+
+        } else {
+
+            int frenteAux = this.frente;
+            s = "[";
+
+            while(frenteAux!=this.fin){
+                s += this.arreglo[frenteAux].toString();
+                frenteAux = (frenteAux + 1) % this.TAMANIO;
+                if(frenteAux!=this.fin){
+                    s += ", ";
+                }
+            }
+            s += "]";
+        }
+        return s;
+
+    }
+
+}
