cf8bfce2391ae99221902f60425edfc274f6b315
fcpp7 <facundo.perez@est.fi.uncoma.edu.ar>
2022-04-25 09:14:56 -0300

    Test lista

---
+++ lineales/tests/lineales/testLista.java
@@ -0,0 +1,36 @@
+package tests.lineales;
+
+import lineales.dinamicas.Lista;
+
+public class testLista {
+
+    public static void main(String[] arg) {
+
+        testLista();
+        System.out.println("==================================================================================== ");
+        
+    }
+
+    public static void testLista(){
+        Lista l1 = new Lista();
+        l1.insertar('A', 1);
+        l1.insertar('B', 2);
+        l1.insertar('C', 3);
+        l1.insertar('D', 4);
+        l1.insertar('E', 5);
+        l1.insertar('F', 6);
+        l1.insertar('G', 7);
+        l1.insertar('H', 8);
+        l1.insertar('I', 9);
+        l1.insertar('J', 10);
+        System.out.println("Lista original"+l1.toString());
+
+
+        System.out.println("Lista multiplos"+l1.obtenerMultiplos(3).toString());
+        
+    
+    }
+
+
+    
+}
